a424b30966aa378952691681deef29d14832fb08
AleenL <784757555@qq.com>
2018-03-22 17:56:01 +0800

    初始化leanCloud

---
+++ new world/app/public/js/leanCloud.js
@@ -0,0 +1,9 @@
+import AV from 'leancloud-storage'
+
+let APP_ID = 'OvVqY6JyIgXMvhPg5WUDCdie-gzGzoHsz';
+let APP_KEY = 'dV4NCQ056iRz1wloKCxBqjlc';
+
+AV.init({
+    appId:APP_ID,
+    appKey:APP_KEY
+});
